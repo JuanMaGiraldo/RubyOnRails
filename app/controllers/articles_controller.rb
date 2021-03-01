f292065197008d0af1d12fff27626128ad797235
JuanMaGiraldo <juanmagiraldo@gmail.com>
2021-03-01 11:16:27 -0500

    Getting started finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "admin", password: "123", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -39,12 +42,11 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     redirect_to root_path
-  end
-  
+  end 
 
   private
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :status)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   include Visible
-  has_many :comments
+  
+  has_many :comments, dependent: :destroy #delete comments when article is deleted.
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
