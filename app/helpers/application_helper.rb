5a2d4b3678704e0e18488c082103ab6e95a875eb
JuanMaGiraldo <juanmagiraldo@gmail.com>
2021-03-10 11:00:48 -0500

    Deleting duplicate code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,3 @@
 module ApplicationHelper
 
-  def logged_in?
-    !!session[:user_id]
-  end
-
-  def current_user
-    if logged_in?
-      @current_user ||= User.find_by_id(session[:user_id])
-    end
-  end
-
 end
