a92a71126856353872545ca1955b47af7ec50c78
JuanMaGiraldo <juanmagiraldo@gmail.com>
2021-03-11 18:01:29 -0500

    fixing database

--- db/migrate/20210305144711_create_relationships.rb
+++ db/migrate/20210305144711_create_relationships.rb
@@ -6,9 +6,9 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
 
       t.timestamps
 
-      add_index :relationships, :follower_id
-      add_index :relationships, :followed_id
-      add_index :relationships, [:follower_id, :followed_id], unique: true # A user can't follow another more than once.
+      #add_index :relationships, :follower_id
+      #add_index :relationships, :followed_id
+      #add_index :relationships, [:follower_id, :followed_id], unique: true # A user can't follow another more than once.
 
     end
   end
